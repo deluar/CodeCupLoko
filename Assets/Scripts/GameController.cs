bdbe56db4f9c79ffc3f5e71ab9c15dc37b5323ef
ehmr <ehmr@cin.ufpe.br>
2016-07-10 11:30:25 -0300

    Correcao do pulo e implementacao da tela inicial

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public string nome;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(Input.GetKey(KeyCode.Return)) {
+            SceneManager.LoadScene(nome);
+        }
+       
+	}
+}
