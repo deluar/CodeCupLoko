a3b738c720af7ac23f8d5123fc9a0636910b88c8
Allysson <alla@cin.ufpe.br>
2016-07-09 21:14:25 -0300

    Adicionando espinhos

--- Assets/Scripts/Espinho.cs
+++ Assets/Scripts/Espinho.cs
@@ -2,24 +2,22 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class blocoColorido : MonoBehaviour {
-
-	public	string	nomeFases;
+public class Espinho : MonoBehaviour {
 
 	// Use this for initialization
+
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
 	void OnTriggerEnter2D(Collider2D col){
-
+		
 		if (col.tag == "Personagem"){
-			SceneManager.LoadScene(nomeFases) ;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name) ;
 		}
 	}
 }
