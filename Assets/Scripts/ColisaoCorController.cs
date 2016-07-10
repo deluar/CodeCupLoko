be34795a617e9df2f1d3f89fc597f7ec9290634a
ehmr <ehmr@cin.ufpe.br>
2016-07-10 00:43:36 -0300

    Adicao do controler de plataforma
    Agora a plataforma desaparece ao trocar de cor;

---
+++ Assets/Scripts/ColisaoCorController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColisaoCorController : MonoBehaviour {
+
+	public bool branco;
+	private fundo fundo;
+	private Rigidbody2D rigidBody;
+	public GameObject	colisor;
+
+	void Start () {
+		fundo = FindObjectOfType (typeof(fundo)) as fundo;
+//		rigidBody = GetComponent<Rigidbody2D>();
+//		rigidBody.collisionDetectionMode = CollisionDetectionMode2D.Discrete;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((branco && fundo.padrao) || (!branco && !fundo.padrao)) {
+			colisor.SetActive (false);
+//			rigidBody.isKinematic = false;
+//			rigidBody.collisionDetectionMode = CollisionDetectionMode2D.Discrete;
+
+		} 
+		else {
+			colisor.SetActive (true);
+//			rigidBody.isKinematic = true;
+//			rigidBody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+		}
+		
+	}
+}
