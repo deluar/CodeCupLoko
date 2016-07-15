bd909837ea6a9b79f62b902b501c5a3d1be5400e
Janaína Oliveira <Janana Oliveira>
2016-07-15 17:35:26 -0300

    Correções e Remoções
    Os prefabs existentes foram corrigidos;
    Cenas lixo foram removidas;

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -143,4 +143,16 @@ public class player : MonoBehaviour {
             isGrounded = false;
         }
     }
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Ground") {
+			isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D col){
+		if (col.gameObject.tag == "Ground") {
+			isGrounded = false;
+		}
+	}
 }
