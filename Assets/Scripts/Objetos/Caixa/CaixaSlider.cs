025d10fa0250fb69b07b75db85c7e63780906789
ehmr <ehmr@cin.ufpe.br>
2016-07-22 20:27:01 -0300

    Correcoes de funcionamento

--- Assets/Scripts/Objetos/Caixa/CaixaSlider.cs
+++ Assets/Scripts/Objetos/Caixa/CaixaSlider.cs
@@ -1,24 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlataformSlider : MonoBehaviour
-{
+public class CaixaSlider : MonoBehaviour {
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.tag == "Personagem" || col.gameObject.tag == "Caixa") // || col.gameObject.tag == "Caixa"
+        if (col.gameObject.tag == "Personagem") // || col.gameObject.tag == "Caixa"
         {
             col.gameObject.transform.parent = gameObject.transform;
             //gameObject.transform.SetParent(col.gameObject.transform);
@@ -27,7 +24,7 @@ public class PlataformSlider : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Personagem" || col.gameObject.tag == "Caixa") // || col.gameObject.tag == "Caixa"
+        if (col.gameObject.tag == "Personagem") // || col.gameObject.tag == "Caixa"
         {
             //gameObject.transform.SetParent(null);
             col.gameObject.transform.parent = null;
