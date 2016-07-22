869bb3ce4dfeeac8cdc633b75d372fb1b371d1d0
Allysson <alla@cin.ufpe.br>
2016-07-22 19:07:44 -0300

    fazendo o reset da caixa ao morrer

---
+++ Assets/Scripts/Objetos/Caixa.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Caixa : MonoBehaviour {
+
+	private Vector3 initialPosition;
+	// Use this for initialization
+	void Start () {
+		initialPosition = this.gameObject.transform.position; // salva a posicao inicial da caixa
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void resetPosition(){ // eh chamada na classe player
+		this.gameObject.transform.position = initialPosition; 
+	}
+}
